13d4665cb05f3b178035ead5774ac326f87477f4
Alyssa Cucunato <acucunato18@gmail.com>
2020-02-12 21:33:01 -0500

    added delete functionality, writes to dbjson, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# note-taker
-Express Note Taker App
+# Express Note Taker
+
+Created during Week 11 of UPenn Coding Bootcamp.
+
+## Overview
+
+## Utilizing the App
+
+- View the app HERE {add link to heroku}
+
+## Demo
+
+![Developer Team Generator](./img/appgif.gif "Developer Team Generator")
+![Developer Team Generator](./img/delpoyed.gif "Developer Team Generator")
+
+## Tech used
+
+- HTML
+- CSS
+- Bootstrap
+- Javascript
+- Node.js
+- NPM modules
+- Express.js
+
+## Contributers
+
+- Alyssa Cucunato
--- apiRoutes.js
+++ apiRoutes.js
@@ -9,9 +9,36 @@ module.exports = function(app) {
   app.post("/api/notes", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
+    let noteID = savedNotes.length.toString();
+    newNote.id = noteID;
     savedNotes.push(newNote);
 
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("New Note saved to db.json file. Your note: ", newNote);
     res.json(savedNotes);
+  });
+
+  //deleting notes with their unique id
+  app.delete("/api/notes/:id", function(req, res) {
+    let deleteNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let targetID = req.params.id;
+
+    console.log(`Deleting note with ID ${targetID}`);
+    for (var i = 0; i < deleteNote.length; i++) {
+      if (deleteNote[i].id == targetID) {
+        deleteNote.splice(i, 1);
+      }
+    }
+    //rewrites ids to increment through again
+    for (i = 0; i < deleteNote.length; i++) {
+      deleteNote[i].id = i;
+    }
+
+    // writes new array
+    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote), err => {
+      if (err) throw err;
+      return;
+    });
+    res.send(req.body);
   });
 };
